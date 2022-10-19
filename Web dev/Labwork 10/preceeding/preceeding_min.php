549d5cff54c5c3b5119ffd72a61c7563b0361f3d
Adele <chindahadele2@gmail.com>
2022-10-19 20:21:47 +0300

    Web Dev Labwork 10

--- Web dev/Labwork 10/preceeding/preceeding_min.php
+++ Web dev/Labwork 10/preceeding/preceeding_min.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="preceeding_min.css" rel="stylesheet">
+    <link href="body.css" rel="stylesheet">
     <title>Result</title>
 </head>
 
