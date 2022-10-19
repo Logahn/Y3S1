4c679a30a00e9e8d03188b940d6e23b25b6ac370
Adele <chindahadele2@gmail.com>
2022-10-19 18:46:36 +0300

    Web Dev Labwork 10

---
+++ Web dev/Labwork 10/preceeding/preceeding_min.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="preceeding_min.css" rel="stylesheet">
+    <title>Result</title>
+</head>
+
+<body class="body">
+    <?php
+    $val[] = $_POST["one"];
+    $val[] = $_POST["two"];
+    $val[] = $_POST["three"];
+    $val[] = $_POST["four"];
+    $val[] = $_POST["five"];
+    $min = $val[0];
+    $sum = 0;
+    $j = false;
+    for ($i = 0; $i < 5; $i++) {
+
+        if ((int)$val[$i] < $min) {
+            $min = $val[$i];
+            $min_index = $i;
+        }
+    }
+
+    echo ("<b>Минимальное значение: " . $min . "</b><br><br>");
+    if ($min_index == 0) {
+        echo ("<b>Предшествующее значение: " . $val[$min_index + 4] . "</b><br><br>");
+    } else {
+        echo ("<b>Предшествующее значение: " . $val[$min_index - 1] . "</b><br><br>");
+    }
+
+
+    ?>
+
+</body>
+
+</html>
